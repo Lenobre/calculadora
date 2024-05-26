6ac1ee4a2b094e75ae104875b5827ebc3b73763a
Lenobre <ordnaelnobre@gmail.com>
2024-05-26 00:02:53 -0300

    Front-end criado

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    colors: {
+      background: "#DAF0FF",
+      "calculator-background": "#17181A",
+      white: "#fff",
+      "primary-dark": "#303136",
+      "primary-blue": "#339DFF",
+      "secondary-blue": "#005DB2",
+      "tertiary-blue": "#1991FF",
+      "quaternary-blue": "#B2DAFF",
+      "primary-gray": "#818181",
+      "secondary-gray": "#A5A5A5",
+      "tertiary-gray": "#616161",
+    },
+    fontFamily: {
+      poppins: ["Poppins"],
+    },
     extend: {},
   },
   plugins: [],
